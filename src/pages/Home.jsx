5d104a3528b537dc6fe974c89f55c214e4889d0e
jsvalenciar <jsvalenciar@users.noreply.github.com>
2025-11-23 17:20:31 -0500

    Remove visible 'Volver al Login' text, keep icon (add aria-label)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -74,11 +74,10 @@ function Home() {
             </Link>
           </div>
         ))}
-        <Link to="/" className="back-link">
+        <Link to="/" className="back-link" aria-label="Volver al Login">
           <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M19 12H5M5 12L12 19M5 12L12 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
-          Volver al Login
         </Link>
       </div>
     </>
